c6a19283a25f1ea808cd63c549bd8faedb1d5c1f
Laura K Boyd <lkboyd@gmail.com>
2015-05-14 20:59:38 -0600

    Initial Set up
    Adding access to NancyTooneDB

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NancyToone.Startup))]
+namespace NancyToone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
